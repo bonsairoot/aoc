d7be86fc59bd3e8bb30175a07363501bb9a50aea
gian <g.wetten@bluewin.ch>
2016-12-09 21:15:35 +0100

    Part II done

--- 9/compression.pl
+++ 9/compression.pl
@@ -6,32 +6,32 @@ use warnings;
 open(my $fh,'<',"aoc9_input.txt") or die "Couldn't find file";
 my $text = <$fh>;
 chomp($text);
+my $text2 = $text;
 my $length = 0;
 
+#Part I
 while($text=~/(.*?)\((\d+)x(\d+)\)/){
     $length+=length($1)+$2*$3;
     #pre+3(for brackets and x) + two numbers in brackets + post
     my $cutlength = length($1)+3+length($2)+length($3)+$2;
-    print "$2,$3,$cutlength\n";
     $text = substr $text,$cutlength;
 }
 $length+=length($text);
 print "$length\n";
 
-#part II
-$length = processLine("X(8x2)(3x3)ABCY");
+#Part II
+$length=processLine($text2);
 sub processLine{
     my ($line) = @_;
-    my $length = 0;
-    while($line=~/(.*?)\((\d+)x(\d+)\)/){
-        print "$line\n";
-        my $index = length($1)+3+length($2)+length($3);
-        my $nextLine = (substr $line, $index, $2)x$3;
-        my $cutlength = length($1)+3+length($2)+length($3)+$2;
-        $length+=length($1)+processLine($nextLine);
-        $line = substr $line,$cutlength;
+    my $result = 0;
+    if($line=~s/(.*?)\((\d+)x(\d+)\)//x){
+        my $repeat = $3;
+        my $nextLine = (substr $line, 0, $2);
+        $line = substr $line,$2;
+        $result = length($1)+$repeat*processLine($nextLine)+processLine($line);
+    }else{
+        $result = length($line);
     }
-    return $length;
+    return $result;
 }
-
 print "$length\n";
