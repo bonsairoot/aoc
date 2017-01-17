08404dc592db1b250235afe0880e81775a6504bb
bonsairoot <g.wetten@bluewin.ch>
2017-01-17 22:01:30 +0100

    day 15 part 2 and problem text

--- 15/chineserem.pl
+++ 15/chineserem.pl
@@ -6,13 +6,18 @@ use ntheory qw/chinese/;
 use List::MoreUtils qw(zip);
 use List::Util qw(pairs);
 
+my $PART = 1; # Change to 2 for part 2
 my (@size, @init);
 open(my $fh,'<',"aoc15_input.txt") or die "Couldn't open input file!";
 while(<$fh>){
-    if($_ =~ /Disc #(\d+) has (\d+).*(\d+)\./){
+    if($_ =~ /Disc #(\d+) has (\d+).* position (\d+)\./){
         $size[$1-1] = $2;
         $init[$1-1] = $3;
     }
+}
+if($PART==2){
+    push @init,0;
+    push @size,11;
 }
 my $length = @init;
 my @diff = map{-$init[$_]-(1..$length)[$_]}(0..$length-1);
