5bd774b71cb115c3ee4d038c747c9962b086c009
gian <g.wetten@bluewin.ch>
2017-01-19 18:40:41 +0100

    day 20 part 2 + problem text

--- 20/blocked.py
+++ 20/blocked.py
@@ -10,15 +10,23 @@ ranges.sort()
 upper_bound = ranges[0][1]
 max_upper = upper_bound
 index = 0
+counter = 0
+first = True
 
-while(True):
+while (True):
     found_entry = False
-    while(ranges[index][0]<=upper_bound+1):
+    while (index < len(ranges) and ranges[index][0] <= upper_bound + 1):
         found_entry = True
-        max_upper = max(ranges[index][1],max_upper)
-        index+=1
+        max_upper = max(ranges[index][1], max_upper)
+        index += 1
     if not found_entry:
-        print ("%d" % (upper_bound+1))
-        break
+        if first:
+            print("Part 1: %d" % (upper_bound + 1))
+            first = False
+        if index >= len(ranges):
+            break
+        counter += 1
+        max_upper = ranges[index][1]
     upper_bound = max_upper
 
+print("Part 2: %d" % counter)
