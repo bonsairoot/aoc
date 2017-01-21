a2a11cfa314a648094d100bd8af40155de021439
bonsairoot <g.wetten@bluewin.ch>
2017-01-21 03:13:50 +0100

    Graph creation, Recursive Successors inc counter next

--- 13/jps/src/main.rs
+++ 13/jps/src/main.rs
@@ -1,4 +1,7 @@
-const SIZE: usize = 10;
+pub mod graph;
+use graph::Graph;
+
+const SIZE: usize = 30;
 
 #[cfg(test)]
 mod tests {
@@ -7,6 +10,7 @@ mod tests {
         assert_eq!(11, ::get_min_path((7, 4), 10));
     }
 }
+
 fn fill_grid(fav: i32) -> [[u8; SIZE]; SIZE] {
     let mut grid = [[0u8; SIZE]; SIZE];
     let loop_size: i32 = (SIZE - 1) as i32;
@@ -19,11 +23,12 @@ fn fill_grid(fav: i32) -> [[u8; SIZE]; SIZE] {
             for c in binary_tile.chars() {
                 sum = sum + c.to_digit(10).unwrap();
             }
-            grid[y_coord as usize][x_coord as usize] = if sum & 1 == 0 { 1 } else { 0 };
+            grid[y_coord as usize][x_coord as usize] = (sum & 1) as u8;
         }
     }
     grid
 }
+
 fn print_grid(grid: &[[u8; SIZE]]) {
     for row in grid {
         for column in row {
@@ -32,11 +37,40 @@ fn print_grid(grid: &[[u8; SIZE]]) {
         println!();
     }
 }
+
 fn get_min_path(goal: (u8, u8), fav: i32) -> u8 {
     let grid = fill_grid(fav);
     print_grid(&grid);
+    let graph = create_graph_no_diagonal(&grid);
+    let successors: Vec<usize> = graph.successors(30).collect();
+    println!("{:?}", successors);
     11
 }
+
+fn create_graph_no_diagonal(grid: &[[u8; SIZE]]) -> Graph {
+    let mut graph = Graph::new();
+
+    for node in 1..(SIZE * SIZE) {
+        graph.add_node();
+    }
+
+    for row in 0..(SIZE - 1) {
+        for column in 0..(SIZE - 1) {
+            if grid[row][column] == 1 {
+                continue;
+            }
+            let current = row * SIZE + column;
+            if row != 0 && grid[row - 1][column] == 0 {
+                graph.add_edge(current - SIZE, current);
+            }
+            if column != 0 && grid[row][column - 1] == 0 {
+                graph.add_edge(current - 1, current);
+            }
+        }
+    }
+    graph
+}
+
 fn main() {
     get_min_path((7, 4), 10);
 }
